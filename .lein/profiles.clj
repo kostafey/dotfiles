4523de53c576a651afb7c1e8125cf8a6116c29c4
kostafey@gmail.com <kostafey@gmail.com>
2021-03-21 14:00:07 +0300

    Remove ensime & update cider

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,5 @@
-{:user {:plugins [[refactor-nrepl "2.4.0"]
-                  ;; [cider/cider-nrepl "0.21.0-SNAPSHOT"]
-                  [cider/cider-nrepl "0.21.1"]
+{:user {:plugins [[refactor-nrepl "2.5.0"]
+                  [cider/cider-nrepl "0.25.0-SNAPSHOT"]
                   ]
         :deploy-repositories [["releases" :clojars]
                               ["snapshots" :clojars]]}
--- .sbt/0.13/plugins/plugins.sbt
+++ .sbt/0.13/plugins/plugins.sbt
@@ -1,5 +1,4 @@
-addSbtPlugin("org.ensime" % "sbt-ensime" % "1.12.14")
-addSbtPlugin("io.get-coursier" % "sbt-coursier" % "1.0.0-RC12")
+addSbtPlugin("io.get-coursier" % "sbt-coursier" % "2.0.0-RC6-8")
 
 addCompilerPlugin(
   "org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full
