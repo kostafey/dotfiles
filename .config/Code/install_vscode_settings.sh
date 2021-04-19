6d6798de5234531f05af0a5bf1e7ac7a191bdbe4
kostafey@gmail.com <kostafey@gmail.com>
2021-04-19 20:59:08 +0300

    Add install_vscode_settings.sh file

---
+++ .config/Code/install_vscode_settings.sh
@@ -0,0 +1,3 @@
+# vscode
+ln -sf /data/dotfiles/.config/Code/User/keybindings.json ~/.config/Code/User/keybindings.json
+ln -sf /data/dotfiles/.config/Code/User/settings.json ~/.config/Code/User/settings.json
