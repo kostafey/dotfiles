e9c74e32678eadd9f35a5ec51d0eaacdf3d7bdc3
kostafey@gmail.com <kostafey@gmail.com>
2021-04-20 15:28:04 +0300

    Add disable_keys.sh script for gnomeShell

---
+++ gnomeShell/disable_keys.sh
@@ -0,0 +1,4 @@
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['']"
