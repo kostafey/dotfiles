9f54b3f880c214f67cdfba2d7dfeb76ef525fa51
kostafey@gmail.com <kostafey@gmail.com>
2022-04-08 14:12:04 +0300

    Add xorg/vscode_maximize.sh

--- xorg/install.sh
+++ xorg/install.sh
@@ -2,6 +2,7 @@
 SRC_XORG_SHELL_SCRIPTS="/data/dotfiles/xorg/"
 DEST_GNOME_SHELL_SCRIPTS="/data/soft/bin/"
 
+ln -sf "${SRC_XORG_SHELL_SCRIPTS}vscode_maximize.sh" "${DEST_GNOME_SHELL_SCRIPTS}vscode_maximize.sh"
 ln -sf "${SRC_XORG_SHELL_SCRIPTS}vscode_window.sh" "${DEST_GNOME_SHELL_SCRIPTS}vscode_window.sh"
 ln -sf "${SRC_XORG_SHELL_SCRIPTS}mucommander_window.sh" "${DEST_GNOME_SHELL_SCRIPTS}mucommander_window.sh"
 ln -sf "${SRC_XORG_SHELL_SCRIPTS}terminal_window.sh" "${DEST_GNOME_SHELL_SCRIPTS}terminal_window.sh"
@@ -11,4 +12,4 @@ ln -sf "${SRC_XORG_SHELL_SCRIPTS}jts_window.sh" "${DEST_GNOME_SHELL_SCRIPTS}jts_
 
 # Fix switch input language
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'ru')]"
-setxkbmap us,ru -option "lv3:ralt_alt,grp:alt_shift_toggle"
+setxkbmap us,ru -option "lv3:ralt_alt,grp:alt_caps_toggle"
---
+++ xorg/vscode_maximize.sh
@@ -0,0 +1,3 @@
+code "$@" &&
+
+wmctrl -r :ACTIVE: -b add,maximized_vert,maximized_horz
