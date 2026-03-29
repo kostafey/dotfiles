549dfc99db2b329bca885161b15f136dff1fa90f
kostafey <kostafey@gmail.com>
2026-03-29 19:45:14 +0200

    Update mucommander settings

--- xorg/mucommander_window.sh
+++ xorg/mucommander_window.sh
@@ -1,2 +1,2 @@
 #/data/dotfiles/xorg/run-or-raise.sh com-mucommander-Launcher.com-mucommander-Launcher muCommander
-/data/dotfiles/xorg/run-or-raise.sh "java-lang-Thread.java-lang-Thread" mucommander
+/data/dotfiles/xorg/run-or-raise.sh "java-lang-Thread.java-lang-Thread" /opt/mucommander-1.4.1-1/mucommander.sh
